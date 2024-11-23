32ec5d818dab4660e7d972bc18fb1b79293e069f
eugenioclrc <eugenioclrc@gmail.com>
2024-11-23 11:01:30 +0700

    .

---
+++ test/R1Feel.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Feel} from "src/round1/Feel/feel.sol";
+
+contract R1FeelTest is Test {
+
+    function setUp() public {
+        
+    }
+
+    function test_Increment() public {
+        
+    }
+}
